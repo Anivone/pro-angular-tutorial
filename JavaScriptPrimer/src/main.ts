a78818746d2363cb01f50426f4997c5c2a6eeb45
Maksym Voloshko <anivone18052001@gmail.com>
2020-07-18 00:58:35 +0300

    JavaScriptPrimer completed

--- JavaScriptPrimer/src/main.ts
+++ JavaScriptPrimer/src/main.ts
@@ -1,12 +1,19 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { TempConverter } from './tempConverter';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+console.log(TempConverter.convertFtoC(38));
+console.log(TempConverter.convertFtoC('38'));
 
-if (environment.production) {
-  enableProdMode();
+let tuple: [string, string, string];
+tuple = ['London', 'raining', TempConverter.convertFtoC('38')];
+
+console.log(`It is ${tuple[2]} degrees C and ${tuple[1]} in ${tuple[0]}`);
+
+const cities: { [index: string]: [string, string] } = {};
+
+cities['London'] = ['raining', TempConverter.convertFtoC('38')];
+cities['Paris'] = ['sunny', TempConverter.convertFtoC('52')];
+cities['Berlin'] = ['snowing', TempConverter.convertFtoC('23')];
+
+for (let key in cities) {
+    console.log(`${key}: ${cities[key][0]}, ${cities[key][1]}`);
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
